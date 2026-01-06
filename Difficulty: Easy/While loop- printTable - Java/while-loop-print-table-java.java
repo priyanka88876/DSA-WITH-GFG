4b469f69e2f25bba28086690eb5ff3fa64a89e05
priyanka88876 <pp6934270@gmail.com>
2026-01-06 20:27:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/While loop- printTable - Java/while-loop-print-table-java.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void calculateMultiples(int n) {
+        // code here
+        int i =10;
+        while(i>0){
+            System.out.print(n*i+" ");
+            i--;
+        }
+        System.out.println();
+    }
+}
