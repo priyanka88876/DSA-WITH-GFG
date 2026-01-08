a9c7a08eb7bd3adbc10c393146947072538dc013
priyanka88876 <pp6934270@gmail.com>
2026-01-08 22:45:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.java
@@ -0,0 +1,18 @@
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        for(int i =0;i<arr.length-1;i++){
+            int minpos =i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minpos]>arr[j]){
+                    minpos=j;
+                }
+            }
+            int temp=arr[minpos];
+            arr[minpos] = arr[i];
+            arr[i] = temp;
+        }
+        
+    }
+    
+}
