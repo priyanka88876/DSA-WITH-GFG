543b3dc63b7318f633078f3032205afc4d39abcc
priyanka88876 <pp6934270@gmail.com>
2026-03-16 22:44:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits/count-digits.java
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -1,18 +1,9 @@
-//Back-end complete function Template for Java
-import java.util.*;
-
-public class Solution {
-    public static void main(String args[]) {
-        // Your Code Here
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int count =0;
-        while(n!=0){
-            int rem = n%10;
-            count++;
-            n= n/10;
-        }
-        System.out.print(count);
-    
+class Solution {
+    public int countDigits(int n) {
+        // code here
+    if(Math.abs(n)<10)
+            return 1;
+            return countDigits(n/10)+1;
+        
     }
 }
