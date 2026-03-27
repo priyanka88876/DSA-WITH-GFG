31bea1d1358db73751dda2ddac6ab1b9fd5ff46b
priyanka88876 <pp6934270@gmail.com>
2026-03-27 20:10:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Queue Traversal/queue-traversal.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    public static void queueTraversal(Queue<Integer> q) {
+        // Your code here
+        for(int x:q)
+        System.out.print(x+" ");
+    }
+}
