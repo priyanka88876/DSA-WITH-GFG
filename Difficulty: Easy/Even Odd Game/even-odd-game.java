1791732027e3ddf5ada99c4e8e459672df47e2d1
priyanka88876 <pp6934270@gmail.com>
2026-04-29 23:22:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Even Odd Game/even-odd-game.java
+++ Difficulty: Easy/Even Odd Game/even-odd-game.java
@@ -1,15 +1,18 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Solution {
-    public static void main(String args[]) {
-        // Your code here
-         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if(a%2 == 0){
-            System.out.print("You");
-        }
-        else{
-            System.out.print("Friend");
-        }
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        if(n % 2 == 1) {
+            System.out.println("You");
+            }
+            else {
+                System.out.println("Friend");
+            }
+
+        sc.close();
     }
 }
