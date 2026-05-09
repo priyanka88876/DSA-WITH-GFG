fd7b43f0d4013a02eb90d5c36f13a33ea410eecf
priyanka88876 <pp6934270@gmail.com>
2026-05-09 19:18:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> frequencyCount(int[] arr) {
+        // code here
+         HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+        for(int i=1; i<=arr.length; i++){
+            if(map.containsKey(i)){
+                list.add(map.get(i));
+            }else{
+                list.add(0);
+            }
+        }
+        return list;
+    
+    }
+}
