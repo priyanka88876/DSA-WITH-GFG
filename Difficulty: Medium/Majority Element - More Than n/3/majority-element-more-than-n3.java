75e08fe9e6a764f53ec2f0749fb6354e18892390
priyanka88876 <pp6934270@gmail.com>
2026-05-20 22:40:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ArrayList<Integer> findMajority(int[] arr) {
+        // Code here
+           ArrayList<Integer> list= new ArrayList<> ();
+        HashMap<Integer,Integer> map= new HashMap<> ();
+        HashSet<Integer> set= new HashSet<> ();
+        for(int a :arr)
+        {
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        for(int a : arr)
+        {
+            if(map.get(a)>arr.length/3 && set.add(a))
+            {
+                list.add(a);
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
