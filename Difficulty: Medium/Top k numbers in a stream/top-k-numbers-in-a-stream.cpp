d546b415d5ed88ffab2cbbe48cd723fa5e63084e
priyanka88876 <pp6934270@gmail.com>
2026-06-06 19:41:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top k numbers in a stream/top-k-numbers-in-a-stream.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    vector<vector<int>> kTop(int k, vector<int>& arr){
+    // code here
+    vector<vector<int>> res;
+        unordered_map<int, int> mp;
+        set<pair<int, int>> s;
+        
+        for(auto i: arr) {
+            vector<int> row;
+            if(mp[i] > 0){
+                s.erase({-mp[i], i});
+            }
+                
+            mp[i]++;
+            s.insert({-mp[i], i});
+            
+            int t = 1;
+            for(auto j: s) {
+                if(t > k)
+                    break;
+                    
+                row.push_back(j.second);
+                t++;
+            }
+            
+            res.push_back(row);
+        }
+
+        return res;
+    }
+
+   };
+
