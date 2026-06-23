a1704e6aeefff32abce39a99490c593621d88ef1
priyanka88876 <pp6934270@gmail.com>
2026-06-23 21:18:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fill Method/fill-method.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void fillArray(int arr[], int k) {
+        // code here
+         for(int i=0;i<arr.length;i++)
+        {  
+            arr[i]=k;
+        }
+
+  
+    }
+}
