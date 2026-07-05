7b898a870e58dcd8a2ed91f0ec9b1e789fab935f
priyanka88876 <pp6934270@gmail.com>
2026-07-05 22:45:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Valid Substring/valid-substring.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxLength(String s) {
+        // code here
+         Stack <Integer> stack=new Stack<>();
+        int maxLen=0;
+        stack.push(-1);
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                stack.push(i);
+            }
+            else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                    
+                }
+                else{
+                    maxLen=Math.max(maxLen,i-stack.peek());
+                }
+            }
+            
+        }
+        return maxLen;
+        
+        
+    }
+}
+
